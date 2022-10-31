90cdf69b0b10963e4d10f56ef861da79ac33a407
DANIEL CRUZ <carlosdanielcruz57@gmail.com>
2022-10-31 17:34:44 -0300

    Acadêmico_stopwacht

---
+++ Cronom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+namespace cronometro
+{
+  class Cronom
+  {
+    static void Main()
+    { 
+      start(); 
+    } 
+    static void start()
+    {  
+      int val=0;// a variavel tem q ser cadastrada fora do loop 
+      int time =10; 
+        while (val<time)
+       { 
+          Console.Clear(); // limpar a tela dando a impressão de contagem e não poluido a tela
+          val++; // no incremento não presciso por "=" posso por apenas "val++" que ja vai add mais 1
+          Console.WriteLine(val);
+          Thread.Sleep(1000);// deixa a aplicação mais lenta esta dentro de System.threading
+       }
+
+    }
+  }
+  
+}
