b6c187422c2a68da5c292486ea29301c2867d63e
DANIEL CRUZ <carlosdanielcruz57@gmail.com>
2022-11-01 17:40:52 -0300

    segunda parte do cronometro

--- Cronom.cs
+++ Cronom.cs
@@ -6,21 +6,47 @@ namespace cronometro
   {
     static void Main()
     { 
-      start(); 
+
+      Start(5);
+      
+      
+      
     } 
-    static void start()
+    static void Start(int time)
     {  
-      int val=0;// a variavel tem q ser cadastrada fora do loop 
-      int time =10; 
+        int val=0;// a variavel tem q ser cadastrada fora do loop 
+      
         while (val<time)
        { 
           Console.Clear(); // limpar a tela dando a impressão de contagem e não poluido a tela
           val++; // no incremento não presciso por "=" posso por apenas "val++" que ja vai add mais 1
           Console.WriteLine(val);
           Thread.Sleep(1000);// deixa a aplicação mais lenta esta dentro de System.threading
+          
        }
-
+       Console.WriteLine("acho q deu certo");
+       Thread.Sleep(2500);
+       
+       Menu();
     }
+    static void Menu()
+       {
+         Console.Clear();
+         Console.WriteLine("s = segundos - ex: 10s = 10 segundos");
+         Console.WriteLine("m = minutos - ex: 10m = 10 minutos");
+         Console.WriteLine("0 = encerrar a aplicação");
+         Console.WriteLine("vamos começar !!");
+
+         string hr = Console.ReadLine().ToLower();// .ToLower = converte letras maiusculas em minusculas
+         char type = char.Parse(hr.Substring(hr.Length-1,1));
+         Console.WriteLine(hr);
+         Console.ReadKey();
+         
+         //string var.Subtring seleciona um caracter na sequencia com os parametros (numero total de caract ,quantidade que será escolhida) ex: string bola .subtring(1,1) == b 1º posição 1 elemento.
+
+         //string var .Length conta a quantidade de caracteres passado como parametro dentro de .substring da a quantidade total de posições deixando mais faciu add ou subtrair a quantidade e poder selecionar a letra q desejo
+       }
+        
   }
   
 }
